0fafa1149a3ac8149ca25a0cf30b36831a1b12d0
markup54 <marzenakupka@tlen.pl>
2025-09-12 14:14:24 +0200

    instrukcja switch

--- src/Main.java
+++ src/Main.java
@@ -45,5 +45,22 @@ public class Main {
         //wyrażenie warunkowe zwraca wartosc
         int roznica = liczbaWpisana>liczbaDoZgadywania ? liczbaWpisana - liczbaDoZgadywania : liczbaDoZgadywania - liczbaWpisana;
         System.out.println(roznica);
+
+        roznica = roznica/10; // ponieważ operacja jest wykonywana na liczbach całkowitych wynik tez jest calkowity
+        System.out.println("Róznica modulo 10 = "+roznica);
+        //instrukcja switch
+        switch (roznica){
+            case 0:
+                System.out.println("Bardzo blisko");
+                break;
+            case 1:
+                System.out.println("Całkiem nieźle");
+                break;
+            case 2:
+                System.out.println("Trochę brakowało");
+                break;
+            default:
+                System.out.println("Może następnym razem będzie lepiej");
+        }
     }
 }
